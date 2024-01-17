40bbac472e7a7fa2061510cfad263a440d43e3f3
centinelaCode <centinela1024tb@gmail.com>
2024-01-17 14:46:57 -0600

    [ADD] se crea la view (register.blade.php) y controller (RegisterController.php)
    => el callback de la route se pasa al controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,11 +11,13 @@
 
       <header class="p-5 border-b bg-white shadow">
          <div class="container mx-auto flex justify-between items-center">
-            <h1 class="text-3xl font-black">Devstagram</h1>
+            <h1 class="text-3xl font-black"><a href="/">Devstagram</a></h1>
 
             <nav class="flex gap-2 items-center">
-               <a href="#" class="font-bold uppercase text-gray-600 text-md">Login</a>
-               <a href="#" class="font-bold uppercase text-gray-600 text-md">Crear Cuenta</a>
+               <a href="#" class="font-bold uppercase text-gray-600 text-sm">Login</a>
+               <a href="/crear-cuenta" class="font-bold uppercase text-gray-600 text-sm">
+                  Crear Cuenta
+               </a>
             </nav>
          </div>
       </header>
