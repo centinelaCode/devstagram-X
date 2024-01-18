9f2e9c4e16327f6f49df39d0d3abd6810ce18b22
centinelaCode <centinela1024tb@gmail.com>
2024-01-17 21:13:38 -0600

    [ADD] se agrega nombre a la route register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 
             <nav class="flex gap-2 items-center">
                <a href="#" class="font-bold uppercase text-gray-600 text-sm">Login</a>
-               <a href="/crear-cuenta" class="font-bold uppercase text-gray-600 text-sm">
+               <a href="{{ route('register') }}" class="font-bold uppercase text-gray-600 text-sm">
                   Crear Cuenta
                </a>
             </nav>
